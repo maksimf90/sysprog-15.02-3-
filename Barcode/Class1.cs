2c4d28f2e87d3fc460509c24c9f20d0a9e38d969
maksimf90 <rikunov95@mail.ru>
2024-02-26 22:26:36 +0300

    Добавьте файлы проекта.

---
+++ Barcode/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+
+
+namespace ClassLibraryBar
+{
+    public class Barcode
+    {
+        public static Bitmap GenerateBarcode(string data, int barHeight, Color backgroundColor, Color barColor, Color textColor)
+        {
+            int digitWidth = 10;  
+            int digitHeight = 20; 
+
+            Bitmap barcodeBitmap = new Bitmap(data.Length * digitWidth, barHeight + digitHeight);
+
+            using (Graphics g = Graphics.FromImage(barcodeBitmap))
+            {
+                g.Clear(backgroundColor);
+
+                SolidBrush barBrush = new SolidBrush(barColor);
+                SolidBrush textBrush = new SolidBrush(textColor);
+
+                for (int i = 0; i < data.Length; i++)
+                {
+                    int x = i * digitWidth;
+                    int y = 0;
+
+                    int digit = int.Parse(data[i].ToString());
+
+                    g.FillRectangle(barBrush, x + (digitWidth - digit), y, digit, barHeight);
+
+                    string digitText = data[i].ToString();
+                    SizeF textSize = g.MeasureString(digitText, SystemFonts.DefaultFont);
+                    float textX = x + (digitWidth - textSize.Width) / 2;
+                    float textY = barHeight;
+                    g.DrawString(digitText, SystemFonts.DefaultFont, textBrush, textX, textY);
+                }
+
+                barBrush.Dispose();
+                textBrush.Dispose();
+            }
+
+            return barcodeBitmap;
+   }
+        }
+}
---
+++ Barcode/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Barcode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Данные для штрих-кода
+            string barcodeData = "12563716235293";
+
+            // Ширина и высота полосок штрих-кода
+            int barWidth = 110;
+            int barHeight = 150;
+
+            // Цвета
+            Color backgroundColor = Color.White;
+            Color barColor = Color.Black;
+            Color textColor = Color.Black;
+
+            // Генерируем штрих-код с помощью вашей библиотеки
+            Bitmap barcodeBitmap = ClassLibraryBar.Barcode.GenerateBarcode(barcodeData, barHeight, backgroundColor, barColor, textColor);
+
+            // Отображаем сгенерированный штрих-код на форме
+            pictureBox1.Image = barcodeBitmap;
+        }
+    }
+}
